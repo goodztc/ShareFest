a963bd234d106ed5f516489db325ccf02f464fc1
jon <zhangtc1@lenovo.com>
2021-06-15 18:04:22 +0800

    Fix bug: npm start error, Error: Most middleware (like compress, errorHandler) is no longer bundled with Express

--- sharefest/server.js
+++ sharefest/server.js
@@ -1,4 +1,7 @@
 var express = require('express');
+var compression = require('compression')
+var errorhandler = require('errorhandler')
+
 var app = express();
 var ws = require('../core/transport/WebSocketServer.js');
 
@@ -25,23 +28,28 @@ if (process.env.REQUIRE_HTTPS) {
 }
 
 app.use(express.json());
-app.use(express.compress());
+//app.use(express.compress());
+app.use(compression());
 app.use(allowCrossDomain);
 app.use(express.static(__dirname + '/public'));
 
 var server;
 var wsPort;
 
-app.configure('development', function () {
-    app.use(express.errorHandler({ dumpExceptions:true, showStack:true }));
+//app.configure('development', function () {
+if (app.get('env') === 'development') {
+    //app.use(express.errorHandler({ dumpExceptions:true, showStack:true }));
+    app.use(errorhandler({ dumpExceptions:true, showStack:true }));
     console.log('listening to port 13337');
     server = app.listen(13337);
     ws.instance.start(tracker.instance, server, null, config.clientTimeout);
 //    signaling.start(server);
     console.log('here I am');
-});
+//});
+}
 
-app.configure('production', function () {
+//app.configure('production', function () {
+if (app.get('env') === 'production') {
     var options = {
         // Important: the following crt and key files are insecure
         // replace the following files with your own keys
@@ -50,7 +58,8 @@ app.configure('production', function () {
             fs.readFileSync('secret/SSLcomAddTrustSSLCA.crt')],
         cert:fs.readFileSync('secret/www_sharefest_me.crt')
     };
-    app.use(express.errorHandler({ dumpExceptions:true, showStack:true }));
+    //app.use(express.errorHandler({ dumpExceptions:true, showStack:true }));
+    app.use(errorhandler({ dumpExceptions:true, showStack:true }));
 //    wsPort = process.env.WS_PORT || 443;
 
     peer5.log('listening to port 80');
@@ -61,6 +70,7 @@ app.configure('production', function () {
 
     ws.instance.start(tracker.instance, server, null, config.clientTimeout);
 //    signaling.start(server);
-});
+//});
+}
 
 router.configure(app, __dirname);
